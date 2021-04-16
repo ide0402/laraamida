864a4efc80af823bce64a637e2850d8d20c3d994
ide0402 <t_0242@yahoo.co.jp>
2021-04-16 10:19:28 +0900

    init

---
+++ database/migrations/2021_03_22_021039_add_column_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('pass');
+            $table->text('integer');
+            $table->text('manager_comment');
+            $table->timestamps();
+            $table->primary('id');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            Schema::dropIfExists('users');
+        });
+    }
+}
