7072906c466ce317a99061b5cc1fdceb513ac147
ide0402 <t_0242@yahoo.co.jp>
2021-04-23 16:17:47 +0900

    アミダ登録画面実装

--- app/Http/Controllers/AmidaController.php
+++ app/Http/Controllers/AmidaController.php
@@ -3,13 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 use JavaScript;
 
 class AmidaController extends Controller
 {
-    public function index($kuji_num)
+    public function index()
     {
-        JavaScript::put(['kuji_num' => $kuji_num, 'kuji_row' => 10]);
+        return view('index');
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(User $user, Request $request)
+    {
+        $user->fill($request->all())->save();
+        return redirect('/'. $user->id);
+    }
+
+    public function showAmida(User $user)
+    {
+        JavaScript::put(['kuji_num' => $user->kuji_num, 'kuji_row' => 10]);
         return view('amidakuji');
     }
 
