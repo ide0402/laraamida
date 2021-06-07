6e3ce3dee14915904732abd4cd58abd795b0fe10
ide0402 <t_0242@yahoo.co.jp>
2021-06-07 17:39:46 +0900

    CSS修正

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,11 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>あみだ</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('/css/create.css') }}">
+        <link rel="stylesheet" href="{{ asset('/CSS/create.css') }}">
 
         <!-- Styles -->
         <style>
